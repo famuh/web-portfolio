36d2c63e26b8335eebf4f86fb41f4792d7cbb216
famuh <famuh79@gmail.com>
2023-12-19 15:45:47 +0700

    ubah dikit

--- main.js
+++ main.js
@@ -1,56 +1,40 @@
+var overview = document.getElementById("overview");
+var experience = document.getElementById("experience");
 
-var overview = document.getElementById("overview")
-var experience = document.getElementById("experience")
+document.addEventListener("DOMContentLoaded", function () {
+  var overviewButton = document.getElementById("overview-button");
+  var experienceButton = document.getElementById("experience-button");
+  var portfolioButton = document.getElementById("portfolio-button");
+  var contactButton = document.getElementById("contact-button");
 
-document.addEventListener("DOMContentLoaded", function() {
-    
-    var overviewButton = document.getElementById("overview-button")
-    var experienceButton = document.getElementById("experience-button")
-    // var profileButton = document.getElementById("profile-button")
-var portfolioButton = document.getElementById("portfolio-button")
-var contactButton = document.getElementById("contact-button")
+  overviewButton.addEventListener("click", function () {
+    var targetOverview = document.getElementById("overview");
 
-    overviewButton.addEventListener("click", function() {
-        var targetOverview = document.getElementById("overview")
+    var targetOffset = targetOverview.offsetTop;
 
-        var targetOffset = targetOverview.offsetTop
-
-        window.scrollTo({
-            top: targetOffset,
-            behavior: "smooth"
-        })
-    })
-
-    experienceButton.addEventListener("click", function() {
-        var targetExperience = document.getElementById("experience")
-        window.scrollTo({
-            behavior: "smooth",
-            top: targetExperience.offsetTop
-        })  
-    })
-
-    contactButton.addEventListener("click", function() {
-        var targetExperience = document.getElementById("contact")
-        window.scrollTo({
-            behavior: "smooth",
-            top: targetExperience.offsetTop
-        })  
-    })
-
-    portfolioButton.addEventListener("click", function() {
-        window.location.href = "portfolio.html";
+    window.scrollTo({
+      top: targetOffset,
+      behavior: "smooth",
     });
-    
+  });
 
+  experienceButton.addEventListener("click", function () {
+    var targetExperience = document.getElementById("experience");
+    window.scrollTo({
+      behavior: "smooth",
+      top: targetExperience.offsetTop,
+    });
+  });
 
+  contactButton.addEventListener("click", function () {
+    var targetExperience = document.getElementById("contact");
+    window.scrollTo({
+      behavior: "smooth",
+      top: targetExperience.offsetTop,
+    });
+  });
 
-    // profileButton.addEventListener("click", function() {
-    //     var targetExperience = document.getElementById("profile")
-    //     window.scrollTo({
-    //         behavior: "smooth",
-    //         top: targetExperience.offsetTop
-    //     })  
-    // })
-
-
-})
+  portfolioButton.addEventListener("click", function () {
+    window.location.href = "/pages/portfolio.html";
+  });
+});
