46e58f16bd42f11797ad72ac2842725e03d8f5fe
famuh <famuh79@gmail.com>
2024-09-27 22:29:20 +0700

    Mengubah asset menjadi online asset

--- pages/js/projects.js
+++ pages/js/projects.js
@@ -1,123 +1,163 @@
+const projects = {
+  android: [
+    {
+      name: "Absence",
+      description:
+        "An application to perform employee attendance by applying geofencing and camera media...",
+      tech: "Flutter, Provider, Google Maps API, Camera, Local Storage, Node.Js, Mongo.db, Figma",
+      image: "../img/absence-preview.png",
+      link: "#",
+    },
+    {
+      name: "Diatrofi",
+      description:
+        "Diatrophy is a mobile application engaged in the health sector...",
+      tech: "Flutter, Provider, Web View, Figma",
+      image: "../img/diatrofi-preview.png",
+      link: "https://github.com/famuh/diatrofi-healthcare",
+    },
+    {
+      name: "Movies App",
+      description:
+        "Movies App provides information to users regarding movies that are currently showing, will be showing and top rated movies. Click on an item to view detailed information of the movie. Users can search for movies through the search menu.",
+      tech: "Flutter, Provider & BloC, Clean Architecture, CI/CD, Dependency Injection, Firebase Crashlytics",
+      image: "../img/none-image-preview.png",
+      link: "#",
+    },
+    {
+      name: "News App",
+      description:
+        "News App provides information on the latest news in Indonesia. Users can read the news by pressing one of the items.",
+      tech: "Flutter, Provider",
+      image: "../img/none-image-preview.png",
+      link: "#",
+    },
+    {
+      name: "Swalokal",
+      description:
+        "Swalokal helps users find out where their desired product is sold by simply entering the image of the product they are looking for.",
+      tech: "Kotlin, XML, FastApi, Python, Flask, TensorFlow, Figma",
+      image: "../img/swalokal-preview.png",
+      link: "#",
+    },
+    {
+      name: "Story App",
+      description:
+        "Story App is a story sharing app that allows users to post photos and captions. Users can also use the map feature to view people's stories from their location.",
+      tech: "Kotlin, XML, Google Maps API, Camera",
+      image: "../img/none-image-preview.png",
+      link: "#",
+    },
+    {
+      name: "GitHub User App",
+      description:
+        "GitHub User is an application that allows users to search for GitHub users and view their profiles. Users can enter a username in the search bar. The application will display a list of users that match the search. Users can then select a user to view their profile.",
+      tech: "Flutter, Provider",
+      image: "../img/none-image-preview.png",
+      link: "#",
+    },
 
-        const projects = {
-            android: [
-                {
-                    name: "Absence",
-                    description: "An application to perform employee attendance by applying geofencing and camera media...",
-                    tech: "Flutter, Provider, Google Maps API, Camera, Local Storage, Node.Js, Mongo.db, Figma",
-                    image: "../img/absence-preview.png",
-                    link: "#"
-                },
-                {
-                    name: "Diatrofi",
-                    description: "Diatrophy is a mobile application engaged in the health sector...",
-                    tech: "Flutter, Provider, Web View, Figma",
-                    image: "../img/diatrofi-preview.png",
-                    link: "https://github.com/famuh/diatrofi-healthcare"
-                },
-                {
-                    name: "Movies App",
-                    description: "Movies App provides information to users regarding movies that are currently showing, will be showing and top rated movies. Click on an item to view detailed information of the movie. Users can search for movies through the search menu.",
-                    tech:"Flutter, Provider & BloC, Clean Architecture, CI/CD, Dependency Injection, Firebase Crashlytics",
-                    image: "../img/none-image-preview.png",
-                    link: "#"
-                },
-                {
-                    name: "News App",
-                    description : "News App provides information on the latest news in Indonesia. Users can read the news by pressing one of the items.",
-                    tech: "Flutter, Provider",
-                    image: "../img/none-image-preview.png",
-                    link: "#"
-                },
-                
-                {
-                    name: "Swalokal",
-                    description : "Swalokal helps users find out where their desired product is sold by simply entering the image of the product they are looking for.",
-                    tech: "Kotlin, XML, FastApi, Python, Flask, TensorFlow, Figma",
-                    image: "../img/swalokal-preview.png",
-                    link: "#"
-                },
-                {
-                    name: "Story App",
-                    description : "Story App is a story sharing app that allows users to post photos and captions. Users can also use the map feature to view people's stories from their location.",
-                    tech: "Kotlin, XML, Google Maps API, Camera",
-                    image: "../img/none-image-preview.png",
-                    link: "#"
-                },
-                {
-                    name: "GitHub User App",
-                    description : "GitHub User is an application that allows users to search for GitHub users and view their profiles. Users can enter a username in the search bar. The application will display a list of users that match the search. Users can then select a user to view their profile.",
-                    tech: "Flutter, Provider",
-                    image: "../img/none-image-preview.png",
-                    link: "#"
-                },
-             
-               
-                // Add more projects here
-            ],
-            website: [
-                {
-                    name: "Covid-19 World Tracker",
-                    description: "A website that helps users to find out Covid-19 disease data from abroad...",
-                    tech: "HTML, CSS, Javascript, Node.Js",
-                    image: "../img/web-covid-preview.png",
-                    link: "https://github.com/famuh/Covid-19-World-Tracker"
-                },
-                {
-                    name: "Flash Market",
-                    description: "a simple marketplace website design with the name Flash Market. This is an assignment for the Front End Web course.",
-                    tech: " HTML, CSS",
-                    image: "../img/web-flashmarket-preview.png",
-                    link: "#"
-                },
-                // Add more projects here
-            ],
-            // backend: [
-            //     {
-            //         name: "Bookshelf API",
-            //         description: "A simple back-end application with a book lending theme created using node.js...",
-            //         tech: "Node.Js, Eslint",
-            //         image: "../img/backend-bookshelf-preview.png",
-            //         link: ""
-            //     },
-            //     // Add more projects here
-            // ],
-            graphicDesign: [
-                "../img/porto/Brosur FAIS ke 7.jpg",
-                "../img/porto/brosur alpha.jpg",
-                "../img/porto/networking and security arc.jpg",
-                
+    // Add more projects here
+  ],
+  website: [
+    {
+      name: "Covid-19 World Tracker",
+      description:
+        "A website that helps users to find out Covid-19 disease data from abroad...",
+      tech: "HTML, CSS, Javascript, Node.Js",
+      image: "../img/web-covid-preview.png",
+      link: "https://github.com/famuh/Covid-19-World-Tracker",
+    },
+    {
+      name: "Flash Market",
+      description:
+        "a simple marketplace website design with the name Flash Market. This is an assignment for the Front End Web course.",
+      tech: " HTML, CSS",
+      image: "../img/web-flashmarket-preview.png",
+      link: "#",
+    },
+    // Add more projects here
+  ],
+  // backend: [
+  //     {
+  //         name: "Bookshelf API",
+  //         description: "A simple back-end application with a book lending theme created using node.js...",
+  //         tech: "Node.Js, Eslint",
+  //         image: "../img/backend-bookshelf-preview.png",
+  //         link: ""
+  //     },
+  //     // Add more projects here
+  // ],
+  graphicDesign: [
+    "https://firebasestorage.googleapis.com/v0/b/angkutin-7fc40.appspot.com/o/fadhil-porto%2Fimage%2FBrosur%20FAIS%20ke%207.jpg?alt=media&token=03fa3bf9-8ce1-47b1-b04a-86bc5fed4269",
+    "https://firebasestorage.googleapis.com/v0/b/angkutin-7fc40.appspot.com/o/fadhil-porto%2Fimage%2Fbrosur%20alpha.jpg?alt=media&token=47dc14a5-1a6a-4ab1-a046-ac6dcb4989ff",
+    "https://firebasestorage.googleapis.com/v0/b/angkutin-7fc40.appspot.com/o/fadhil-porto%2Fimage%2Fnetworking%20and%20security%20arc.jpg?alt=media&token=e81374de-fc3d-40d9-9c69-45ded4fa2df0",
 
-                "../img/porto/pengabdian.jpg",
-                "../img/porto/data science.jpeg",
-                "../img/porto/feed ig bitsmikro.jpg",
+    "https://firebasestorage.googleapis.com/v0/b/angkutin-7fc40.appspot.com/o/fadhil-porto%2Fimage%2Fpengabdian.jpg?alt=media&token=3e5f1bc8-c5da-4c8a-9de6-233a7e859db6",
+    "https://firebasestorage.googleapis.com/v0/b/angkutin-7fc40.appspot.com/o/fadhil-porto%2Fimage%2Fdata%20science.jpeg?alt=media&token=293742ee-510a-43e0-9bc8-ecfa34beb8ce",
+    "https://firebasestorage.googleapis.com/v0/b/angkutin-7fc40.appspot.com/o/fadhil-porto%2Fimage%2Ffeed%20ig%20bitsmikro.jpg?alt=media&token=f5132970-089f-4499-982f-c2e94b82034f",
 
+    "https://firebasestorage.googleapis.com/v0/b/angkutin-7fc40.appspot.com/o/fadhil-porto%2Fimage%2FLife%20After%20Bangkit.png?alt=media&token=51e61467-97ad-4ab0-aaef-08ac09cfda96",
+    "https://firebasestorage.googleapis.com/v0/b/angkutin-7fc40.appspot.com/o/fadhil-porto%2Fimage%2FIG%20BITS%201%20OKT.jpg?alt=media&token=8dbdef8d-b99d-4792-b708-7c758e08a4ee",
 
-                "../img/porto/Life After Bangkit.png",
-                "../img/porto/IG BITS 1 OKT.jpg",
+    // Add more images here
+  ],
+  videography: [
+    {
+      title: "Bangkit 2023 Experience",
+      description:
+        "Bangkit Academy 2023 led By Google, GoTo, Traveloka batch 1 Experience as Mobile Development participant",
+      link: "https://firebasestorage.googleapis.com/v0/b/angkutin-7fc40.appspot.com/o/fadhil-porto%2Fvideo%2Fssstik.io_%40famooh_1727426543477.mp4?alt=media&token=a6bf4abc-90e6-4b36-a751-acf7d8db4dab",
+    },
+    {
+      title: "Kampus Merdeka X Studi Independen Batch 3 Dicoding",
+      description:
+        "Video experience as Multi-Platform and Back-End participant at Dicoding Indonesia",
+      link: "https://firebasestorage.googleapis.com/v0/b/angkutin-7fc40.appspot.com/o/fadhil-porto%2Fvideo%2Fssstik.io_1727426522602.mp4?alt=media&token=b90392fe-feb2-4b6a-b021-434505b43f2c",
+    },
+    {
+      title: "Video Assignment for Negotiation Techniques (2) College Course",
+      description: "",
+      link: "https://firebasestorage.googleapis.com/v0/b/angkutin-7fc40.appspot.com/o/fadhil-porto%2Fvideo%2FTugas%202%20Negosiasi%20Qyta.mp4?alt=media&token=6c85de36-4880-470d-9e8f-0e3337708ca4",
+    },
 
+    {
+      title: "Freelance Video Editor",
+      description: "",
+      link: "https://firebasestorage.googleapis.com/v0/b/angkutin-7fc40.appspot.com/o/fadhil-porto%2Fvideo%2FFIX.m4v?alt=media&token=7771b249-7ce9-49bb-8627-5205202f747a",
+    },
+    {
+      title: "Video Assignment for Negotiation Techniques (1) College Course",
 
+      description: "",
+      link: "https://firebasestorage.googleapis.com/v0/b/angkutin-7fc40.appspot.com/o/fadhil-porto%2Fvideo%2FTeknik%20Negosiasi_Tugas%201.mp4?alt=media&token=b39bcd76-9195-4ef3-85e8-268ab1b3c4eb",
+    },
+    {
+      title: "Self-introduction video for entering campus organizations",
+      description: "",
+      link: "https://firebasestorage.googleapis.com/v0/b/angkutin-7fc40.appspot.com/o/fadhil-porto%2Fvideo%2Fperkenalan%20bitsmikro.mp4?alt=media&token=d8580da9-910b-444a-a1ef-192b45bdb5d0",
+    },
+    // "https://www.youtube.com/embed/JIQGVRRAKv4",
+    // Add more video links here
+  ],
+};
 
-
-                // Add more images here
-            ],
-            videography: [
-                "https://www.youtube.com/embed/y3_3LQppNS8",
-                // "https://www.youtube.com/embed/JIQGVRRAKv4",
-                // Add more video links here
-            ]
-        };
-
-        // Function to render projects
-        function renderProjects(containerId, projectList, isVideo = false) {
-            const container = document.getElementById(containerId);
-            projectList.forEach(project => {
-                let item = document.createElement('div');
-                item.classList.add('grid-item');
-                if (isVideo) {
-                    item.innerHTML = `<iframe width="320" height="240" src="${project}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
-                } else if (typeof project === 'object') {
-                    item.innerHTML = `
+// Function to render projects
+function renderProjects(containerId, projectList, isVideo = false) {
+  const container = document.getElementById(containerId);
+  projectList.forEach((project) => {
+    let item = document.createElement("div");
+    item.classList.add("grid-item");
+    if (isVideo) {
+      item.innerHTML = `
+                <iframe width="320" height="240" src="${project.link}" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                   <div class="video-information">
+                    <p>${project.title}</p>
+                    <p>${project.description}</p>
+                   </div>
+                    `;
+    } else if (typeof project === "object") {
+      item.innerHTML = `
                         <div class="portfolio-section-1">
                             <div class="foto-portfolio">
                                 <a href="${project.link}">
@@ -133,16 +173,18 @@
                             </div>
                         </div>
                     `;
-                } else {
-                    item.innerHTML = `<img src="${project}" alt="" srcset="">`;
-                }
-                container.appendChild(item);
-            });
-        }
+    } else {
+      item.innerHTML = `
+      <a href="${project}" target="_self"><img src="${project}" alt="" srcset=""></a>
+      `;
+    }
+    container.appendChild(item);
+  });
+}
 
-        // Render all project sections
-        renderProjects('androidProjects', projects.android);
-        renderProjects('websiteProjects', projects.website);
-        // renderProjects('backendProjects', projects.backend);
-        renderProjects('graphicDesign', projects.graphicDesign);
-        renderProjects('videography', projects.videography, true);
+// Render all project sections
+renderProjects("androidProjects", projects.android);
+renderProjects("websiteProjects", projects.website);
+// renderProjects('backendProjects', projects.backend);
+renderProjects("graphicDesign", projects.graphicDesign);
+renderProjects("videography", projects.videography, true);
